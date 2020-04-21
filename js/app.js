c4b6eee52ca2402a904aa3e9c49a660930b58682
francoing <franco.montti.19@gmail.com>
2020-04-21 16:40:37 -0300

    configurado app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-// var url = window.location.href;
-// var swLocation = '/Twittor/sw.js'
+var url = window.location.href;
+var swLocation = '/portfolio/sw.js'
 
-// if (navigator.serviceWorker) {
-
-//     if (url.includes('localhost')) {
-//         swLocation = '/sw.js';
-//     }
-//     navigator.serviceWorker.register(swLocation);
-// }
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+
+    if (url.includes('localhost')) {
+        swLocation = '/sw.js';
+    }
+    navigator.serviceWorker.register(swLocation);
 }
+// if (navigator.serviceWorker) {
+//     navigator.serviceWorker.register('/sw.js');
+// }
